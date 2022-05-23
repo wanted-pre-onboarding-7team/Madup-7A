20b1a968859c3d449477456c867d1d0d8fd609e4
Leejin Yang <78616893+Leejin-Yang@users.noreply.github.com>
2022-05-23 12:50:41 +0900

    Layout (#1)

---
+++ src/routes/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import styles from './dashboard.module.scss'
+
+const Dashboard = () => {
+  return <div className={styles.container}>dashboard</div>
+}
+
+export default Dashboard
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,18 @@
-import styles from './routes.module.scss'
+import { Route, Routes } from 'react-router-dom'
+
+import Layout from './_shared/Layout'
+import DashBoard from './Dashboard'
+import AdsManage from './AdsManage'
 
 const App = () => {
-  return <div className={styles.app} />
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path='/' element={<DashBoard />} />
+        <Route path='ads/manage' element={<AdsManage />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
